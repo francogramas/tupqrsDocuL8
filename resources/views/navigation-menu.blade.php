0bf324555973184ba182d8dd4e735b4c9568bb4e
Francisco Moreno <francogramas@gmail.com>
2022-11-16 08:58:09 -0500

    ExpedienteMenu

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -60,9 +60,9 @@
                         <x-jet-nav-link href="{{ route('lider') }}" :active="request()->routeIs('lider')">
                             {{ __('Respuestas') }}
                         </x-jet-nav-link>
-                        <!--x-jet-nav-link href="{{ route('expediente') }}" :active="request()->routeIs('expediente')">
+                        <x-jet-nav-link href="{{ route('expediente') }}" :active="request()->routeIs('expediente')">
                             {{ __('Expediente') }}
-                        </!--x-jet-nav-link-->
+                        </x-jet-nav-link>
                         @endrole
                         @role('Admin')
                         <x-jet-nav-link href="{{ route('empresas') }}" :active="request()->routeIs('empresas')">
@@ -114,9 +114,9 @@
                                             <x-jet-dropdown-link href="{{ route('lider') }}">
                                                 {{ __('Respuesta') }}
                                             </x-jet-dropdown-link>
-                                            <!--x-jet-dropdown-link href="{{ route('expediente') }}">
+                                            <x-jet-dropdown-link href="{{ route('expediente') }}">
                                                 {{ __('Expediente') }}
-                                            </!--x-jet-dropdown-link-->
+                                            </x-jet-dropdown-link>
                                         @endrole
                                         @role('Admin')
                                             <x-jet-dropdown-link href="{{ route('empresas') }}">
@@ -253,9 +253,9 @@
                     <x-jet-responsive-nav-link href="{{ route('lider') }}" :active="request()->routeIs('lider')">
                         {{ __('Respuestas') }}
                     </x-jet-responsive-nav-link>
-                    <!--x-jet-responsive-nav-link href="{{ route('expediente') }}" :active="request()->routeIs('expediente')">
+                    <x-jet-responsive-nav-link href="{{ route('expediente') }}" :active="request()->routeIs('expediente')">
                         {{ __('Expediente') }}
-                    </!--x-jet-responsive-nav-link-->
+                    </x-jet-responsive-nav-link>
                 @endrole
                 @hasanyrole('Gerente|Ventanilla')
                     <x-jet-responsive-nav-link href="{{ route('ventanilla') }}" :active="request()->routeIs('ventanilla')">
